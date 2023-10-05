49f11fac95978562ab433078acea044e693d4aed
Toremi9 <142730474+Toremi9@users.noreply.github.com>
2023-10-05 21:25:26 +0200

    Update main.js
    cleaned the code and created more functions for readability

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
 const choice = ["rock", "paper", "scissors"];
 let myPoints = 0;
 let pcPoints = 0;
-let result = "";
+
+game();
+
 
 function getComputerChoice() {
-    let pcChoice = choice[(Math.floor(Math.random() * choice.length))]
-    return pcChoice;
+    return choice[(Math.floor(Math.random() * choice.length))]
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
+    let choice = playerSelection.toLowerCase();
+    if (choice === "rock" && computerSelection === "paper" || choice === "paper" && computerSelection === "scissors" || choice === "scissors" && computerSelection === "rock"){
         pcPoints += 1;
-        console.log("My points = " + myPoints);
-        console.log("PC points = " + pcPoints);
-        return "You lost this one, but don't give up!";
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper" || playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors" || playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
-        console.log("My points = " + myPoints);
-        console.log("PC points = " + pcPoints);
-        return "It's a draw, you can get him next time!";
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" || playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
+    } else if (choice === "scissors" && computerSelection === "paper" || choice === "rock" && computerSelection === "scissors" || choice === "paper" && computerSelection === "rock"){
         myPoints += 1;
-        console.log("My points = " + myPoints);
-        console.log("PC points = " + pcPoints);
-        return "You won! now do it again!";
-    }
-  }
+    } 
+}
 
 function game() {
     for (i = 0; i < 5; i++) { 
-        const computerSelection = getComputerChoice(); 
+        let computerSelection = getComputerChoice(); 
         let playerSelection = prompt("Choose rock, paper or scissors");
-        console.log("My choice = " + playerSelection);
-        console.log("Opponent's choice = " + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-
+        playRound(playerSelection, computerSelection);
+        if(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+            showChoice(playerSelection, computerSelection);
+            showPoints();
+        } else {
+            console.log("Wrong value");
+        }
     }
-    if (myPoints > pcPoints) {
-        result = "You won the ROCK PAPER SCISSORS!";
-    }
-    else if (myPoints < pcPoints) {
-        result = "You lost ROCK PAPER SCISSORS!";
-    }
-    else if (myPoints == pcPoints){
-        result = "It's a draw!";
-    }
-    console.log(result);
+    showResult();
 }
 
-  game();
+function showChoice(playerSelection, computerSelection) {
+    console.log("My choice = " + playerSelection);
+    console.log("Opponent's choice = " + computerSelection);
+}
+
+function showPoints() {
+    console.log("My points = " + myPoints);
+    console.log("PC points = " + pcPoints);
+}
+
+function showResult() {
+    let result;
+    if (myPoints > pcPoints) {
+        result = "You won the ROCK PAPER SCISSORS!";}
+    else if (myPoints < pcPoints) {
+        result = "You lost ROCK PAPER SCISSORS!";}
+    else if (myPoints == pcPoints){
+        result = "It's a draw!";}
+
+    console.log(result);
+    return result;
+}
 
