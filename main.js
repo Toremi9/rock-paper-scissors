d2cadaa67fe03055e7df0138e6c18977b04ae66e
Toremi9 <142730474+Toremi9@users.noreply.github.com>
2023-10-05 19:35:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,19 +13,18 @@ function playRound(playerSelection, computerSelection) {
         pcPoints += 1;
         console.log("My points = " + myPoints);
         console.log("PC points = " + pcPoints);
-        return "You lost this one, but don't give up!";}
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper" || playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors" || playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
+        return "You lost this one, but don't give up!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper" || playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors" || playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
         console.log("My points = " + myPoints);
         console.log("PC points = " + pcPoints);
         return "It's a draw, you can get him next time!";
-    }
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" || playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" || playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
         myPoints += 1;
         console.log("My points = " + myPoints);
         console.log("PC points = " + pcPoints);
-        return "You won! now do it again!";}
+        return "You won! now do it again!";
+    }
   }
-
 
 function game() {
     for (i = 0; i < 5; i++) { 
@@ -47,7 +46,6 @@ function game() {
     }
     console.log(result);
 }
-
 
   game();
 
